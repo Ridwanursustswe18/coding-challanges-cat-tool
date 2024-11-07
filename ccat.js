79894ae029db3ec852600661ece043282c93d693
Ridwan <ridwanur196@gmail.com>
2024-11-07 12:00:54 +0600

    reading from standart input implemented

--- ccat.js
+++ ccat.js
@@ -1,6 +1,20 @@
 const numberOfArguments = process.argv.length - 2;
 const fs = require('fs');
-if(numberOfArguments == 1){
+const readline = require('node:readline');
+if(numberOfArguments == 1 && process.argv[2] == '-'){
+    const readLine = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        
+    });
+    let inputData = '';
+
+  readLine.on('line', (line) => {
+    inputData += line + '\n';
+  });
+
+}
+else if(numberOfArguments == 1){
     const fileName = process.argv[2];
     fs.readFile(fileName, 'utf8', (err, data) => {
         try{
